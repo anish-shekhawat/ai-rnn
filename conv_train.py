0674761c6729a963e202c14b1d413c457305781f
anish-shekhawat <anish.s.shekhawat@gmail.com>
2017-11-21 17:45:28 -0800

    Adds functions to construct NN layers

--- conv_train.py
+++ conv_train.py
@@ -141,12 +141,98 @@ class CONVNET(object):
             label[i] = label[j]
             label[j] = temp
 
-    def __initialize_variable(self, mode):
+    def train(self, model_file='trained_model', data=None, model_params=None):
+        """Trains the Neural Net on data folder
+
+        :param model_file: File to which model is written
+        :param data: Input data
+        :param model_params: Tensorflow Model parameters
+        :returns: Time taken to train
+        """
+
+        self.__initialize_variable()
+
+    def __initialize_variable(self):
         """Initializes Tensorflow variable
 
         :param mode: Mode in which script is run (train,test,k-fold)
         :returns: Train step, input, label, accuracy Tensor depending on mode
         """
+
+        # Input
+        x = tf.placeholder(tf.float32, [None, 625], name='x')
+
+        # Label
+        y = tf.placeholder(tf.float32, [None, 2], name="y")
+
+        network = self.__get_network_details()
+
+        # Dictionary to store weights and biases
+        W = {}
+        b = {}
+        prev_dim = 1
+
+        for i in range(len(network) - 1):
+            W[i], b[i] = self.__get_wb(i, network[i][0], prev_dim,
+                                       network[i][1], 'conv2d')
+
+    def __get_network_details(self):
+        """Fetches Network details from Network Description file
+
+        :returns: List of network details
+        """
+        out = []
+
+        with open(sys.argv[2]) as net_file:
+            for line in net_file.readlines():
+                out.append(map(int, line.split()))
+
+        return out
+
+    def __get_wb(self, index, kernel, prev_dim, features, layer):
+        """Returns Weight and Bias TF variables
+
+        """
+        weight = 'W' + str(index)
+        bias = 'b' + str(index)
+        initializer = tf.contrib.layers.xavier_initializer()
+
+        if layer == 'conv2d':
+            shape = [kernel, kernel, prev_dim, features]
+        else:
+            shape = [kernel * kernel * prev_dim, features]
+
+        W = tf.get_variable(
+            weight, shape, dtype=tf.float32, initializer=initializer)
+
+        b = tf.get_variable(bias, [features], dtype=tf.float32,
+                            initializer=initializer)
+
+        return W, b
+
+    def __get_conv(self, input_x, weight, bias, kernel):
+        """Returns a convolutional network layer"""
+
+        layer = tf.nn.conv2d(input_x, weight, strides=[1, kernel, kernel, 1],
+                             padding='SAME')
+        layer = tf.nn.bias_add(layer, bias)
+        layer = tf.nn.relu(layer)
+
+        size = [1, 2, 2, 1]
+        return tf.nn.max_pool(layer, ksize=size, strides=size, padding='SAME')
+
+    def __get_dense(self, X, weights, bias, activation):
+        """Returns a dense layer"""
+
+        node = tf.add(tf.matmul(X, weights), bias)
+
+        if activation == 'relu':
+            return tf.nn.relu(node)
+        elif activation == 'sigmoid':
+            return tf.nn.sigmoid(node)
+
+        return node
+
 
 if __name__ == '__main__':
     # Check if correct number of arguments passed
@@ -161,3 +247,5 @@ if __name__ == '__main__':
     CONV = CONVNET(sys.argv[1], sys.argv[3], sys.argv[4], sys.argv[5])
 
     CONV.set_inputs(sys.argv[7])
+
+    CONV.train(sys.argv[2])
